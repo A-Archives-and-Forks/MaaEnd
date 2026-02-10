f89fb8b46f5ca6d62958b8a735bc35a4f6fe6e4c
weinibuliu <weinibuliu@outlook.com>
2026-02-10 22:38:05 +0800

    rft: rename

--- agent/go-service/keymap/register.go
+++ agent/go-service/keymap/register.go
@@ -3,8 +3,8 @@ package keymap
 import maa "github.com/MaaXYZ/maa-framework-go/v4"
 
 func Register() {
-	maa.AgentServerRegisterCustomAction("_ClickKey", &_ClickKey{})
-	maa.AgentServerRegisterCustomAction("_LongPressKey", &_LongPressKey{})
-	maa.AgentServerRegisterCustomAction("_KeyDown", &_KeyDown{})
-	maa.AgentServerRegisterCustomAction("_KeyUp", &_KeyUp{})
+	maa.AgentServerRegisterCustomAction("km:ClickKey", &_ClickKey{})
+	maa.AgentServerRegisterCustomAction("km:LongPressKey", &_LongPressKey{})
+	maa.AgentServerRegisterCustomAction("km:KeyDown", &_KeyDown{})
+	maa.AgentServerRegisterCustomAction("km:KeyUp", &_KeyUp{})
 }
